1006feda8246565f449a4cd6ac535b79bbe8a049
Brudlord <brudloord@yandex.ru>
2024-10-27 22:31:44 +0300

    Make password restore

--- app/src/main/java/com/example/mobileproject/SignIn.java
+++ app/src/main/java/com/example/mobileproject/SignIn.java
@@ -1,24 +1,75 @@
 package com.example.mobileproject;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.example.mobileproject.databinding.ActivitySignInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignIn extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
+    private String TAG = "SignInTag";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_sign_in);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        mAuth = FirebaseAuth.getInstance();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            goToMainActivity();
+        }
+        binding.signInBtn.setOnClickListener(v -> signInUser());
+
+        binding.goToSingUpTv.setOnClickListener(v -> goToSignUp());
+
+        binding.forgotPasswordTv.setOnClickListener(v -> goToRestorePassword());
     }
+
+    private void signInUser() {
+        String email = binding.userEt.getText().toString();
+        String password = binding.passwordEt.getText().toString();
+
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(getApplicationContext(), "Empty fields are not allowed", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        goToMainActivity();
+                    } else {
+                        Exception e = task.getException();
+                        Log.w(TAG, "signInResult:failed " + e.toString());
+                        Toast.makeText(getApplicationContext(), "Authentication failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void goToMainActivity() {
+        startActivity(new Intent(SignIn.this, MainActivity.class));
+    }
+    private void goToSignUp() {
+        startActivity(new Intent(SignIn.this, SignUp.class));
+    }
+
+     private void goToRestorePassword() {
+         startActivity(new Intent(this, RestorePassword.class));
+     }
 }
--- app/src/main/java/com/example/mobileproject/User.java
+++ app/src/main/java/com/example/mobileproject/User.java
@@ -1,19 +1,19 @@
-package com.example.friendlocation.utils;
+package com.example.mobileproject;
 
-import java.util.ArrayList;
 import java.util.Objects;
 
 public class User {
     public String name;
     public String email;
     public String id;
-    public int right;
-    public boolean isVisible = true;
-    public Pair<Double, Double> coordinates = null;
 
     public User() {
     }
 
+
+    public User(String id) {
+        this.id = id;
+    }
     public User(String name, String email) {
         this.name = name;
         this.email = email;
@@ -25,22 +25,19 @@ public class User {
         this.id = id;
     }
 
-    public User(String id, Integer right) {
-        this.id = id;
-        this.right = right;
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         User user = (User) o;
-        return right == user.right && Objects.equals(name, user.name) && Objects.equals(email, user.email) && Objects.equals(id, user.id) && Objects.equals(coordinates, user.coordinates);
+        return Objects.equals(name, user.name) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(id, user.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, email, id, right, coordinates);
+        return Objects.hash(name, email, id);
     }
 }
 
